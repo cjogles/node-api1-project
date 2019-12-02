64bc45663db7e04ef94ec4ec3ab3abf82d3c84c0
cjogles <jackson.ogles28@gmail.com>
2019-12-02 15:45:17 -0700

    post request modified

--- index.js
+++ index.js
@@ -7,23 +7,29 @@ server.use(express.json())
 server.get('/', (req, res) => {
     res.send({api:"API up and running..."});
 })
-    // | POST   | /api/users     | Creates a user using the information sent inside the `request body`.  
+// | POST   | /api/users     | Creates a user using the information sent inside the `request body`.  
 server.post('/api/users', (req, res) => {
     const userData = req.body;
-    db.insert(userData)
-        .then(user => {
-            res
-                .status(201)
-                .json(user);
-        })
-        .catch(error => {
-            console.log('error creating user from information requested inside req body', error);
-            res
-                .status(500)
-                .json({errorMessage: 'Error creating user from information requested'})
+        db.insert(userData)
+            .then(user => {
+                if (user.name && user.bio) {
+                    res
+                        .status(201)
+                        .json(user);
+                } else {
+                    res
+                        .status(400)
+                        .json({errorMessage: 'Please provide name and bio for the user.' })
+                }
+            })
+            .catch(error => {
+                console.log('error creating user from information requested inside req body', error);
+                res
+                    .status(500)
+                    .json({error: 'There was an error while saving the user to the database'})
         })
 })
-    // | GET    | /api/users     | Returns an array of all the user objects contained in the database. 
+// | GET    | /api/users     | Returns an array of all the user objects contained in the database. 
 server.get('/api/users', (req, res) => {
     db.find()
         .then(users => {
@@ -38,7 +44,7 @@ server.get('/api/users', (req, res) => {
                 .json({errorMessage: 'Error getting all users from data base'})
         })
 })
-    // | GET    | /api/users/:id | Returns the user object with the specified `id`.          
+// | GET    | /api/users/:id | Returns the user object with the specified `id`.          
 server.get('/api/users/:id', (req, res) => {
     const id = req.params.id;
     db.findById(id)
@@ -54,7 +60,7 @@ server.get('/api/users/:id', (req, res) => {
                 .json({errorMessage: 'Error getting user by id'})
         })
 })
-    // | DELETE | /api/users/:id | Removes the user with the specified `id` and returns the deleted user.    
+// | DELETE | /api/users/:id | Removes the user with the specified `id` and returns the deleted user.    
 server.delete('/api/users/:id', (req, res) => {
     const id = req.params.id;
     db.remove(id)
@@ -76,7 +82,7 @@ server.delete('/api/users/:id', (req, res) => {
                 .json({errorMessage: 'Error deleting user by id'})
         })
 })          
-    // | PUT    | /api/users/:id | Updates the user with the specified `id` using data from the `request body`. Returns the modified document, **NOT the original**. |
+// | PUT    | /api/users/:id | Updates the user with the specified `id` using data from the `request body`. Returns the modified document, **NOT the original**. |
 server.put('/api/users/:id', (req, res) => {
     const id = req.params.id;
     const updates = req.body;
